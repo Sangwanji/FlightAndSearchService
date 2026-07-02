305cf031894fcc972721efd33e640196389db993
Sangwanji <sakshamsangwan2005@outlook.com>
2026-07-03 03:20:25 +0530

    Set up sequelize

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express=require('express');
-const PORT=require('./config/serverconfig').PORT;
-const setUpAndServer =async()=>{
-    const app=express();
-    const Port=PORT;
 
-    app.listen(Port,()=>{
-        console.log(`Server is running on port ${Port}`);
+const {PORT} =require('./config/serverConfig');
+const setUpAndServer =async()=>{
+    // create express app
+    const app=express();
+
+    //body parser middleware
+    app.use(express.json());
+
+    // start the server
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
     })
 }
 
