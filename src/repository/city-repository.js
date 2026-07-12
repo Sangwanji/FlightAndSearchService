4b9638c1baeb8e4727e3603e2e4b46233e16bced
Sangwanji <sakshamsangwan2005@outlook.com>
2026-07-12 20:32:29 +0530

    Minor fixes UpdateCity in city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -25,11 +25,16 @@ class CityRepository {
     }
     async updateCity(cityId,data){
         try{
-            const city=await City.update(data,{
-                where:{
-                    id:cityId
-                }
-            });
+            // below apprach also work but note return updated object is not possible in this approach
+            // if we are using postgres then we can use returning:true in the update method to get the updated object
+            // const city=await City.update(data,{
+            //   where:{
+            //   id:cityId
+            //   }
+            // });
+            const city=await City.findByPk(cityId);
+            city.name=data.name;
+            await city.save();
             return city;
         }catch(error){
             console.log("Something went wrong in the repository layer");
