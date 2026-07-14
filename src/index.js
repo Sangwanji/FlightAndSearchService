3e824392f54f7fca45d10d92ce87434afaf44246
Sangwanji <sakshamsangwan2005@outlook.com>
2026-07-14 13:03:57 +0530

    Add seeder and associate the table

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express=require('express');
 
 const {PORT} =require('./config/serverConfig');
 const ApiRoutes=require('./routes/index');
+
+const db= require('./models/index');
+const {City , Airport} =require('./models/index');
+
 const setUpAndServer =async()=>{
     // create express app
     const app=express();
@@ -13,8 +17,19 @@ const setUpAndServer =async()=>{
     app.use('/api',ApiRoutes);
 
     // start the server
-    app.listen(PORT,()=>{
+    app.listen(PORT,async ()=>{
         console.log(`Server is running on port ${PORT}`);
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter:true});
+        }
+        const city=await City.findOne({
+            where:{
+                id:7
+            }
+        });
+        const airports=await city.getAirports();
+        console.log(city);
+        console.log(airports);
     })
 }
 
