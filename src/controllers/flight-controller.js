b7c4d5f975ae52e150494cf5884948664f92fb3d
Sangwanji <sakshamsangwan2005@outlook.com>
2026-07-16 00:45:24 +0530

    Add filter at fligh controller so only req things pass from req.body to service

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -4,7 +4,16 @@ const flightServiceInstance=new FlightService();
 
 const create=async(req,res)=>{
     try{
-        const flight=await flightServiceInstance.createFlight(req.body);
+        const flightRequestData={
+            flightNumber:req.body.flightNumber,
+            airplaneId: req.body.airplaneId,
+            departureAirportId :req.body.departureAirportId,
+            arrivalAirportId : req.body.arrivalAirportId,
+            arrivalTime:req.body.arrivalTime,
+            departureTime: req.body.departureTime,
+            price: req.body.price
+        }
+        const flight=await flightServiceInstance.createFlight(flightRequestData);
         return res.status(201).json({
             data:flight,
             success:true,
